3ddff29ae52efe142781aeb83f06b88af511a695
Lianyuji <wz121b96ap@outlook.com>
2026-02-13 09:42:52 +0800

    Completed Chapter 7 - Category pages with slug URLs and page listing

--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True, blank=True)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+
+
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
